1b3554a9c5f756d72d611e1ca80dd93922233b47
Thatz Weerakoon <59426115+thatz98@users.noreply.github.com>
2021-01-07 00:03:40 +0530

    added certificate section

--- public/js/custom.js
+++ public/js/custom.js
@@ -391,3 +391,10 @@ $(function(){
     });
 
 });
+
+$(document).ready(function($){
+  $("#show-more-btn").click(function(e){
+    $(".show-more-item:hidden").slice(0,6).fadeIn();
+    if ($(".show-more-item:hidden").length < 1) $(this).fadeOut();
+  })
+})
